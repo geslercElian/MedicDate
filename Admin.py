de2e32e65201294a6eee0583a85633f8dd3834c5
geslercalero1@gmail.com <geslercalero1@gamil.com>
2026-09-06 16:53:54 -0600

    Faltante de funciones

--- Admin.py
+++ Admin.py
@@ -1,1 +1,7 @@
 print('Administrador')
+print("""
+Funciones que ha de tener el admin:
+1. Crear usuarios, tanto pacientes como medicos.
+2. Definir horarios en que los medicos esten disponibles.
+3. Hacer reporte y estadistica de las citas... y entre otras tareas que vengan con sus ideas
+""")
