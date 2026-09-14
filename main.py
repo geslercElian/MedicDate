a08c22e207cc0b46cdaa671dfa5a4278c12378ee
geslercalero1@gmail.com <geslercalero1@gamil.com>
2026-09-14 17:20:42 -0600

    ...

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 #PROYECTO INTEGRADOR
 #DESAROLLO DE UN PROGRAMA EN LNG PYTHON ENFOCADO EN LA GESTION DE CITAS MEDICAS EN PUESTOS DE SALUD DE LA CIUDAD DE LEON
 
+import  Medico, Admin, mensajes, sqlite3
+
+conexion = sqlite3.connect('dataBase.db')
+
+
 print("BIENVENID@ AL SISTEMA DE GESTION DE CITAS MEDICAS")
+
+Usuario = str(input('Ingrese su usuario: '))
+
+contraseña = str(input('Ingrese su contraseña')) 
+
 
